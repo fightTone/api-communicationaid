d0f5fdea8802367f2c82ffe3ff5daa2c57a1566b
Lyn Albacete <lynalbct.gmail.com>
2018-04-10 20:35:24 -0300

    Create API for login

--- api/models.py
+++ api/models.py
@@ -20,7 +20,7 @@ report = db.Table('report',
 class Account(db.Model):
     acc_id = db.Column(db.Integer, primary_key=True)
     acc_type = db.Column(db.Integer, unique=True)
-    username = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(60), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(150), unique=True)
     acc_p = db.relationship("Parent", uselist=False, backref="account")
