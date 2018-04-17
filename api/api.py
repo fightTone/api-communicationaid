3dc8ab1cbf3a23da0b8b50c3f328a9c6a184dcbf
unknown <yumielouise@gmail.com>
2018-04-17 23:37:14 +0800

    added api for posting progress reports

--- api/api.py
+++ api/api.py
@@ -14,9 +14,22 @@ def getoneuser(acc_id):
     user = Account.query.filter_by(acc_id=acc_id).first()
     if not user:
         return jsonify({'message: "no user found"'})
-    user_data = {}
+    user_data = {}                          #container
     user_data['acc_id'] = user.acc_id
-    user_data['username'] = user.username
+    user_data['username'] = user.username  #dictionary
     user_data['email'] = user.email
     user_data['acc_type'] = user.acc_type
     return jsonify({'user': user_data})
+
+@app.route('/api/educational/progress/', methods=['POST'])
+def progress(ed_num):
+    report = Progress.query.filter_by(ed_num=ed_num).first()
+
+    prog_data = {}
+    prog_data['title'] = report.title
+    prog_data['details'] = report.details
+    prog_data['score'] = report.score
+
+    db.session.add(Progress)
+    db.session.commit()
+    return jsonify({'data': prog_data, 'status': 'ok'})
