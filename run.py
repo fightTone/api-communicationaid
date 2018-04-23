c8388917968768a599d532762206f5317ce790b2
Lyn Albacete <lynalbct.gmail.com>
2018-04-23 03:40:22 -0300

    Fixed run.py

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app import app
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
